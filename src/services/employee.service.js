49338159410fa83f7eba12669ea5eb52ccbd33d7
Jonel Muncan <jonelm1901@gmail.com>
2023-05-10 16:47:58 -0400

    Employee && Task Services
    CRUD + Find services

---
+++ src/services/employee.service.js
@@ -0,0 +1,33 @@
+import http from "../http-common";
+
+class EmployeeDataService {
+  getAll() {
+    return http.get("/employees");
+  }
+
+  get(id) {
+    return http.get(`/employees/${id}`);
+  }
+
+  create(data) {
+    return http.post("/employees", data);
+  }
+
+  update(id, data) {
+    return http.put(`/employees/${id}`, data);
+  }
+
+  delete(id) {
+    return http.delete(`/employees/${id}`);
+  }
+
+  deleteAll() {
+    return http.delete(`/employees`);
+  }
+
+  findByTitle(first_name) {
+    return http.get(`/employees?first_name=${first_name}`);
+  }
+}
+
+export default new EmployeeDataService();
