078d1b6bb194d18fd6a9da8a7e597377cf8b1d2e
albertochan81 <alberto.chanliu81@myhunter.cuny.edu>
2023-05-18 16:52:31 -0400

    case Url update 4

--- src/http-common.js
+++ src/http-common.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 export default axios.create({
   URL: "/api",
-  baseURL: "https://final-backened.herokuapp.com/api"||"http://localhost:8080/api",
+  baseURL: "https://final-backened.herokuapp.com/api",
+  //baseURL: "http://localhost:80/api",
   headers: {
     "Content-type": "application/json"
   }
