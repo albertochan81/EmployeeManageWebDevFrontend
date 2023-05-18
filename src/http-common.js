d556c603d4fb7d94d498298c1c475671f3d97080
albertochan81 <alberto.chanliu81@myhunter.cuny.edu>
2023-05-18 19:47:45 -0400

    server update test updates

--- src/http-common.js
+++ src/http-common.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default axios.create({
   URL: "/api",
-  baseURL: "https://repo-test1.herokuapp.com/api",
+  baseURL: "https://final-backened.herokuapp.com/api",
   //baseURL: "http://localhost:80/api",
   headers: {
     "Content-type": "application/json"
