d9f6aa468cfe856a86ae5b5c5ce3f65cc4fc871f
albertochan81 <alberto.chanliu81@myhunter.cuny.edu>
2023-05-18 21:20:10 -0400

    Small frontend changes

--- src/http-common.js
+++ src/http-common.js
@@ -3,7 +3,6 @@ import axios from "axios";
 export default axios.create({
   URL: "/api",
   baseURL: "https://final-backened.herokuapp.com/api",
-  //baseURL: "http://localhost:80/api",
   headers: {
     "Content-type": "application/json"
   }
